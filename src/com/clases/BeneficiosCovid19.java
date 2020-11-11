d29012f5391f8f7404622c1280be24606592300d
SOAM\1758828 <cardona.james@tcs.com>
2020-11-10 22:58:39 -0500

    actualización wrapper

--- src/com/clases/BeneficiosCovid19.java
+++ src/com/clases/BeneficiosCovid19.java
@@ -34,7 +34,7 @@ public class BeneficiosCovid19 {
     //Implementar un metodo que retorne el mejor beneficio comparando los valores de todos los beneficios
     //public Float getMejorbeneficios(){
     //    return Float.valueOf(0);
-    //}
+    //
 
 	
 }
